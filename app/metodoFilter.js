4378e225f278f59b5dea84e4a47db77b43ee2aa0
Matheus <matheus.domingues126@gmail.com>
2023-04-11 12:46:31 -0300

    adicioanr métodos reduce e sort

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,7 +4,26 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const data = elementoBtn.dataset.filtro
-    console.log(data);
-    const livrosFiltrados = livros.filter(livro => livro.categoria === data)
-    console.table(livrosFiltrados)
+    const livrosFiltrados = data === 'disponivel' ? filtrarDisponivel() : filtrarCategoria(data)
+    exibirLivros(livrosFiltrados)
+    if(data === 'disponivel') {
+        const valorTotal = calcularLivrosTotais(livrosFiltrados)
+        exbibirValoTotal(valorTotal)
+    }
+}
+
+function filtrarCategoria(data) {
+    return livros.filter(livro => livro.categoria === data);
+}
+
+function filtrarDisponivel() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exbibirValoTotal(valorTotal) {
+    elementoValorTotalDeLivros.innerHTML = `
+        <div class="livros__disponiveis">
+            <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+        </div>
+    `
 }
--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,10 +1,15 @@
 const elementoParaInserirLivros = document.getElementById('livros')
+const elementoValorTotalDeLivros = document.getElementById('valor_total_livros_disponiveis')
 
 function exibirLivros(livros) {
+    elementoValorTotalDeLivros.innerHTML = '';
+    elementoParaInserirLivros.innerHTML = '';
     livros.forEach(livro => {
+        //let disponibilidade = verificarDisponibilidadeDoLivro(livro)
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
         elementoParaInserirLivros.innerHTML += `
             <div class="livro">
-                <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+                <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
                 <h2 class="livro__titulo">
                     ${livro.titulo}
                 </h2>
@@ -17,3 +22,11 @@ function exibirLivros(livros) {
         `
     });
 }
+
+// function verificarDisponibilidadeDoLivro(livro) {
+//     if(livro.quantidade > 0) {
+//         return 'livro__imagens'
+//     } else {
+//         return 'livro__imagens indisponivel'
+//     }
+// }
